f5a27bd74642975d93b2c0fba51c307d27c25c70
Andrew Kochulab <macinsiderorg@gmail.com>
2016-01-29 15:33:53 +0200

    Added project

---
+++ EduTeacher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EduTeacher
+//
+//  Created by Andrew Kochulab on 29.01.16.
+//  Copyright © 2016 Andrew Kochulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
